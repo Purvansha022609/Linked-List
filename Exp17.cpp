50bd815f69e5f110dfb0df496346ad3a5d96a6e9
Purvansha gehlod <139473344+Purvansha022609@users.noreply.github.com>
2023-10-19 17:57:06 +0530

    Update Exp17.cpp

--- Exp17.cpp
+++ Exp17.cpp
@@ -24,3 +24,50 @@ int main(){
 /*Output
 1 0
 */
+
+/*
+Name-Purvansha Gehlod
+PRN-22070123088
+Exp-17(Par-B)
+*/
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;  // Use '=' instead of '*'
+        next = NULL;
+    }
+};
+
+void insertAtHead(Node*& head, int val) {
+    Node* new_node = new Node(val);
+    new_node->next = head;  // Update the new node's next pointer
+    head = new_node;  // Update the head to point to the new node
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = NULL;
+    insertAtHead(head, 2);
+    display(head);
+    insertAtHead(head, 1);
+    display(head);
+    return 0;
+}
+/*Output
+2 -> NULL
+1 -> 2 -> NULL
+*/
